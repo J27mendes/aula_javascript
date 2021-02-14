079b27701c8857d8b110b425e8648b5993bba621
J27mendes <71888684+J27mendes@users.noreply.github.com>
2021-02-14 20:57:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "obrigado por clicar";
+}
+
+function redirecionar(){
+    window.open("https://www.linkedin.com/");
+   // window.location.href = "https://www.linkedin.com/feed/"; 
+    }
+    function trocar(elemento){
+        //document.getElementById("mousemove").innerHTML = "obrigado por mudar o texto";
+    elemento.innerHTML = "obrigado por mudar o texto";
+    }
+
+    function voltar(elemento){
+       //document.getElementById("mousemove").innerHTML = "passe o mouse aqui";
+    elemento.innerHTML = "passe o  mouse aqui";
+    }
+
+    function funcaoChange(elemento){
+        console.log(elemento.value);
+    }
+/*function soma (n1, n2){
+    return n1 + n2;
+}
+
+alert (soma(10, 56));
+---------------------------
+var validar
+function validaIdade(idade) {
+    if (idade >= 18) {
+        validar = true
+    }else{validar  = false}
+    return validar; 
+}
+
+var idade = prompt ("Qual a sua idade?");
+validaIdade(idade);
+console.log(validar);*/0
+
+
+
+/*var nome = "Jessé Mendes"
+var n1 = 40;
+//alert(nome + " tem " + idade + " anos");
+var n2 = 15;
+var frase = "Brasil é o melhor time do mundo";
+console.log(nome);
+console.log(n1/n2);
+console.log(frase.replace("Brasil", "Holanda"));
+console.log(frase.toLowerCase());*/
+
+//var lista = ["chun-li", "cammy", "iroha", "rimururu"]
+//console.log(lista);
+//lista.push("charlote");
+//console.log(lista);
+//lista.pop();
+//console.log(lista);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+//var garotas = [{nome:"chun-li", cor:"branca"}, {nome:"sakura", cor:"branca"}]
+//console.log(garotas);
+//alert (garotas[1].nome + garotas[0].nome);
+
+
+/*var idade = prompt("qual sua idade");
+//var idade = 18;
+ if (idade > 18) {
+     alert ("maior de idade");
+ }else{alert ("menor de idade")};*/
+
+ /*var count = 0;
+ while (count <= 5){
+     console.log(count);
+     alert (count);
+ count++
+};*/
+
+/*var count
+for (count = 0; count<5; count++ ) {
+    alert (count);
+};*/
+
+/*var d = new Date
+alert (d.getHours());
+alert (d.getMonth());
+alert (d.getTime());*/
